9157d68eb7f8cdc19a27ab8826331cc23c91560e
Park Ryuck <maeshmallow@gmail.com>
2021-11-11 19:37:46 +0900

    Initial commit

---
+++ calcClock.js
@@ -0,0 +1,32 @@
+
+//12시 38분
+function calcTotal(hourValue,minValue) {
+    //시침의 각도 구하는 함수 --> 입력시 12  출력각도
+    function calcHour(hour) {
+        return (360/12) * (hour % 12)
+    }
+
+//분침의 각도 구하는 함수
+// const calcMin = min => 360/60) * min
+    const calcMin = function(min) {
+        return (360/60) * min
+    }
+
+//분침의 이동에 따른 시침의 이동 각도 구하는 함수
+    function getExtra (min) {
+        return (30/60) * min
+
+    }
+
+    const result = Math.abs((calcHour(hourValue) + getExtra(minValue)) - calcMin(minValue))
+
+    return result > 180 ? 360-result:result
+}
+
+console.log(calcTotal(12,38))
+
+
+
+
+
+
